1e555bdb3d4856f62dd8ff53f4992c96ed8f53ca
deriancampos <71363391+DerianCampos@users.noreply.github.com>
2026-05-10 17:41:16 -0600

    feat: adding forgot and reset response

--- src/features/auth/passwordApi.ts
+++ src/features/auth/passwordApi.ts
@@ -4,21 +4,30 @@ interface IForgotPasswordRequest {
   email: string;
 }
 
+interface IForgotPasswordResponse {
+  message: string;
+  token: string;
+}
+
 interface IResetPasswordRequest {
   token: string;
   password: string;
 }
 
+interface IResetPasswordResponse {
+  message: string;
+}
+
 export const passwordApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
-    forgotPassword: builder.mutation<{ message: string }, IForgotPasswordRequest>({
+    forgotPassword: builder.mutation<IForgotPasswordResponse, IForgotPasswordRequest>({
       query: (body) => ({
         url: "/auth/forgot-password",
         method: "POST",
         body,
       }),
     }),
-    resetPassword: builder.mutation<{ message: string }, IResetPasswordRequest>({
+    resetPassword: builder.mutation<IResetPasswordResponse, IResetPasswordRequest>({
       query: (body) => ({
         url: "/auth/reset-password",
         method: "POST",
