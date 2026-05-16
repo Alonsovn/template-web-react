083346278fa44d4e7c8a30e4822e3175b56236cc
Génesis Morales <genesismoralesc1@gmail.com>
2026-05-16 15:48:31 -0600

    improve auth slice typings and typed hooks

--- src/features/auth/authSlice.ts
+++ src/features/auth/authSlice.ts
@@ -3,24 +3,32 @@ import type { PayloadAction } from "@reduxjs/toolkit";
 
 interface IAuthState {
   username: string | null;
+  token: string | null;
+  isAuthenticated: boolean;
 }
 
 const initialState: IAuthState = {
   username: null,
+  token: null,
+  isAuthenticated: false,
 };
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
   reducers: {
-    setUsername: (state, action: PayloadAction<string>) => {
-      state.username = action.payload;
+    setCredentials: (state, action: PayloadAction<{ username: string; token: string }>) => {
+      state.username = action.payload.username;
+      state.token = action.payload.token;
+      state.isAuthenticated = true;
     },
     logout: (state) => {
       state.username = null;
+      state.token = null;
+      state.isAuthenticated = false;
     },
   },
 });
 
-export const { setUsername, logout } = authSlice.actions;
+export const { setCredentials, logout } = authSlice.actions;
 export default authSlice.reducer;
