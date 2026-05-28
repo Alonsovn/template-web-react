07365842fa8138d21f688e5f71412fd0c7949260
Génesis Morales <genesismoralesc1@gmail.com>
2026-05-28 15:08:00 -0600

    feat(app): integrate auth routing and protected navigation

--- src/shared/store/store.ts
+++ src/shared/store/store.ts
@@ -1,11 +1,11 @@
-import { configureStore } from "@reduxjs/toolkit";
-import { apiSlice } from "../api/apiSlice";
-import authReducer from "../../features/auth/model/authSlice";
+import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from '../api/apiSlice';
+import authReducer from '../../features/auth/model/authSlice';
 
 export const store = configureStore({
   reducer: {
-    [apiSlice.reducerPath]: apiSlice.reducer,
     auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(apiSlice.middleware),
